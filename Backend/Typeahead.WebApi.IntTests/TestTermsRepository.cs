7fa9738788ec03b2d2dce954a568efc3c761dde4
Perju Marcel <marcel.perju@sonova.com>
2021-11-16 16:48:22 +0100

    Add WeightControllerTests

---
+++ Backend/Typeahead.WebApi.IntTests/TestTermsRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Typeahead.WebApi.Data;
+
+namespace Typeahead.WebApi.IntTests
+{
+    public class TestTermsRepository : TermsRepository
+    {
+        private readonly TermsDataOptions _options;
+
+        public TestTermsRepository(TermsDataOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        public async Task<Weight> GetWeight(int termId, string input)
+        {
+            await using SqlConnection con = new SqlConnection(_options.ConnectionString);
+
+            string sqlQuery = "SELECT * FROM Weights WHERE TermId=@TermId AND Input=@Input";
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+            cmd.Parameters.AddWithValue("@TermId", termId);
+            cmd.Parameters.AddWithValue("@Input", input);
+
+            await con.OpenAsync();
+            SqlDataReader rdr = await cmd.ExecuteReaderAsync();
+
+            var weight = new Weight();
+            while (rdr.Read())
+            {
+                weight.TermId = Convert.ToInt32(rdr["TermId"]);
+                weight.Input = rdr["Input"].ToString();
+                weight.Count = Convert.ToInt32(rdr["Count"]);
+            }
+
+            await con.CloseAsync();
+
+            return weight;
+        }
+    }
+}
---
+++ Backend/Typeahead.WebApi.IntTests/WeightControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Typeahead.WebApi.Data;
+using Typeahead.WebApi.Model;
+using Xunit;
+
+namespace Typeahead.WebApi.IntTests
+{
+    public class WeightControllerTests : IntegrationTestsBase<Startup>
+    {
+        [Theory]
+        [InlineData(5, "mic")]
+        [InlineData(2, "mic")]
+        [InlineData(4, "mic")]
+        [InlineData(2, "micr")]
+        [InlineData(3, "micr")]
+        public async Task IncreaseWeightNoContent(int termId, string input)
+        {
+            var currentWeight = await WeightCount(termId, input);
+
+            var response = await PostAsJsonAsync(termId, input);
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+            var newWeight = await WeightCount(termId, input);
+            newWeight.Count.Should().Be(currentWeight.Count + 1);
+        }
+
+        [Theory]
+        [InlineData(0, "mic")]
+        [InlineData(-1, "mic")]
+        [InlineData(1, "")]
+        [InlineData(1, "m")]
+        [InlineData(1, "mi")]
+        [InlineData(1, null)]
+        [InlineData(1, "verylongscringabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")]
+        public async Task IncreaseWeightBadRequest(int termId, string input)
+        {
+            var response = await PostAsJsonAsync(termId, input);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        private async Task<Weight> WeightCount(int termId, string input)
+        {
+            var options = Services.GetService<TermsDataOptions>();
+            if (options == null)
+            {
+                throw new NullReferenceException(nameof(options));
+            }
+
+            var repository = new TestTermsRepository(options);
+            return await repository.GetWeight(termId, input);
+        }
+
+        private async Task<HttpResponseMessage> PostAsJsonAsync(int termId, string input)
+        {
+            var weightInput = new WeightInput { TermId = termId, Input = input };
+            var json = JsonConvert.SerializeObject(weightInput);
+            var stringContent = new StringContent(json, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var response = await Client.PostAsync("/weight", stringContent);
+            return response;
+        }
+    }
+}

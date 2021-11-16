29d1502aa68dc576ab5d4afa5c4348ec2975fc54
Perju Marcel <marcel.perju@sonova.com>
2021-11-16 16:36:21 +0100

    Add integration tests for TermsController

---
+++ Backend/Typeahead.WebApi.IntTests/IntegrationTestsBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Typeahead.WebApi.IntTests
+{
+    public abstract class IntegrationTestsBase<TStartup> : IAsyncLifetime where TStartup : class
+    {
+        private readonly WebApplicationFactory<TStartup> _factory;
+
+        protected virtual string GetTestSettingsJsonPath()
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.integrationtests.json");
+            return configPath;
+        }
+
+        protected IntegrationTestsBase() : this(typeof(TStartup).Assembly.GetName().Name)
+        {
+        }
+
+        protected IntegrationTestsBase(string projectRelativePath)
+        {
+            _factory = new AsyncFriendlyWebApplicationFactory<TStartup>().WithWebHostBuilder(
+                b =>
+                {
+                    b.UseSolutionRelativeContentRoot(projectRelativePath).UseEnvironment("Local").ConfigureAppConfiguration(
+                        (ctx, builder) =>
+                        {
+                            var configPath = GetTestSettingsJsonPath();
+                            if (!string.IsNullOrWhiteSpace(configPath))
+                            {
+                                builder.AddJsonFile(configPath, optional: true);
+                            }
+                        });
+                });
+
+            Client = _factory.CreateDefaultClient(new Uri("http://example.com/"));
+            Services = _factory.Services;
+        }
+
+        protected IServiceProvider Services { get; }
+
+        protected HttpClient Client { get; }
+
+        public virtual Task InitializeAsync() => Task.CompletedTask;
+
+        public virtual Task DisposeAsync()
+        {
+            _factory.Dispose();
+            return Task.CompletedTask;
+        }
+
+        private class AsyncFriendlyWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
+        {
+            protected override IHost CreateHost(IHostBuilder builder)
+            {
+                var host = builder.Build();
+                Task.Run(() => host.StartAsync()).GetAwaiter().GetResult();
+                return host;
+            }
+        }
+    }
+}
---
+++ Backend/Typeahead.WebApi.IntTests/TermsControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Typeahead.WebApi.Model;
+using Xunit;
+
+namespace Typeahead.WebApi.IntTests
+{
+    public class TermsControllerTests : IntegrationTestsBase<Startup>
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("v")]
+        [InlineData("ve")]
+        [InlineData("nonexistingword")]
+        [InlineData("verylongstringabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]
+        public async Task GetFilteredTermsNotFound(string filter)
+        {
+            var response = await Client.GetAsync("/terms/" + filter);
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Theory]
+        [InlineData("micr", 10)]
+        public async Task GetFilteredTermsSuccess(string filter, int expectedCount)
+        {
+            var response = await Client.GetAsync("/terms/" + filter);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var rawContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Term>>(rawContent);
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+            result.Should().HaveCount(expectedCount);
+            result.Should().NotContain(x => !x.Name.Contains(filter));
+        }
+    }
+}

70af76d100a09d9eaa48d3e48f1fa5b87ae95a7b
Perju Marcel <marcel.perju@sonova.com>
2021-11-16 17:26:36 +0100

    Add term.service.ts and add the calls in component

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { Term } from 'src/models/term';
+import { TermService } from 'src/services/term.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'typeahead';
+  public terms: Term[];
+
+  constructor(private _termService: TermService) {
+  }
+
+  getTerms(filter: string) {
+    if(filter.length < 3) {
+      return;
+    }
+
+    this._termService.getTerms(filter).subscribe(
+      (data: Term[]) => this.terms = data
+    );
+  }
+
+  weightIncrease(term: Term) {
+    this._termService.postWeightIncrease(term.id, 'mic').subscribe((data: any) => console.log(data));
+  }
 }
